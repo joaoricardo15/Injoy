b005cf891c58f194f5c98a391c3197e65fcd25b8
Joao Cardoso <joao.cardoso@franq.com.br>
2020-04-09 13:52:59 -0300

    adição de pedido de instalação

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SwipeableViews from "react-swipeable-views";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import { Button } from "@material-ui/core";
+import GetAppIcon from "@material-ui/icons/GetApp";
 import NewsPage from "./pages/news/news.page";
 import PageMemes from "./pages/memes/memes.page";
 import TweetsPage from "./pages/tweets/tweets.page";
 import "./App.css";
 
 const App = () => {
+  const [installation, setInstallation] = useState(null);
   const [tab, setTab] = useState(0);
 
   const handleTabChange = (event, tabValue) => {
@@ -19,9 +22,41 @@ const App = () => {
     setTab(tabValue);
   };
 
+  const showInstalattionPainel = (event) => {
+    event.preventDefault();
+    setInstallation({ event, installButton: true });
+  };
+
+  const installApp = () => {
+    installation.event.prompt();
+    installation.event.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") setInstallation(null);
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) =>
+      showInstalattionPainel(e)
+    );
+  }, []);
+
   return (
     <div className="container">
       <div>
+        {installation && (
+          <div
+            style={{ display: "flex", justifyContent: "flex-end", padding: 10 }}
+          >
+            <Button
+              color="primary"
+              variant="outlined"
+              startIcon={<GetAppIcon />}
+              onClick={installApp}
+            >
+              Instalar
+            </Button>
+          </div>
+        )}
         <AppBar position="static" color="default">
           <Tabs
             value={tab}
