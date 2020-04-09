2c3b7a0aec7f80618f78b5114ec75e2adb4ed008
Joao Cardoso <joao.cardoso@franq.com.br>
2020-04-09 17:31:58 -0300

    add in site request for news

--- src/services/request.service.js
+++ src/services/request.service.js
@@ -6,12 +6,50 @@ const serverUrl = "https://injoy.us-east-1.elasticbeanstalk.com";
 const serverRequest = Request.create({ baseURL: serverUrl });
 
 export const getNews = (keyWord, category) => {
-  return serverRequest.request({
-    url: "/news",
-    params: {
-      q: keyWord,
-      category,
-    },
+  const newsApiUrl = "https://newsapi.org/v2/";
+  const newsApiToken = "ed62172b723f4a598514e9a0aec3d2ae";
+  const newsApiHeaders = { Accept: "application/json" };
+
+  let url;
+  const params = {
+    apiKey: newsApiToken,
+    pageSize: 30,
+  };
+
+  if (keyWord) {
+    url = "everything";
+    params.q = keyWord;
+    params.language = "pt";
+    params.sortBy = "popularity";
+  } else {
+    url = "top-headlines";
+    params.q = "a";
+
+    if (category !== "world") {
+      params.country = "br";
+      params.language = "pt";
+    }
+
+    if (category !== "world" && category !== "brasil") {
+      params.category = category;
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    Request.request({
+      url: url,
+      baseURL: newsApiUrl,
+      headers: newsApiHeaders,
+      params: params,
+    })
+      .then((result) => {
+        if (result.status === 200) {
+          resolve(result.data.articles);
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
   });
 };
 
